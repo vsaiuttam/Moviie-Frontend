266d17dbb040144fcb91c7ecae10838f1fc29f8c
vsaiuttam <saiuttam2000@gmail.com>
2024-04-24 13:49:23 +0530

    m

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,17 +1,18 @@
 export interface MovieCardType {
     title: string;
-    portraitImgUrl: string;
-    landscapeImgUrl: string;
+    portraitImgUrl?: string;
+    landscapeImgUrl?: string;
     _id: string;
     rating: number;
-    genre: string[];
-    description: string;
-    duration: number;
-    cast: CelebrityCardType[];
-    crew: CelebrityCardType[];
-    imageUrl: string[];
-    type:string[];
+    genre?: string[];
+    description?: string;
+    duration?: number;
+    cast?: CelebrityCardType[];
+    crew?: CelebrityCardType[];
+    imageUrl: string;
+    type: string;
 }
+
 
 export interface CelebrityCardType{
     name: string;
@@ -19,3 +20,4 @@ export interface CelebrityCardType{
     _id: string;
     role: string;
 }
+
