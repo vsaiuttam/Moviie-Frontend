f85f532abfc8f3edf0606ee3fe431b7488b29b73
vsaiuttam <saiuttam2000@gmail.com>
2024-04-24 13:35:22 +0530

    Update MovieCarousel.tsx

--- src/components/moviecarousel/MovieCarousel.tsx
+++ src/components/moviecarousel/MovieCarousel.tsx
@@ -7,7 +7,7 @@ import { Pagination } from 'swiper/modules';
 import MovieCard from './MovieCard';
 import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
 const MovieCarousel= ()=>{   
-    const Movies: MovieCardType[]=[
+    const Movies: MovieCard[]=[
         {
             title:"Dj Tillu",
             imageUrl:"https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:l-image,i-discovery-catalog@@icons@@star-icon-202203010609.png,lx-24,ly-615,w-29,l-end:l-text,ie-OS4xLzEwICA5NksgVm90ZXM%3D,fs-29,co-FFFFFF,ly-612,lx-70,pa-8_0_0_0,l-end/et00343267-ucfqbyhxcu-portrait.jpg",
@@ -83,9 +83,25 @@ return (
                 {
                     Movies.map((Movie)=>{
                         return(
-                            <SwiperSlide>
-                                <MovieCard{...Movie}/>
-                            </SwiperSlide>
+                            <Swiper
+                slidesPerView={1}
+                spaceBetween={1}
+                pagination={{ clickable: true }}
+                breakpoints={{
+                    '@0.00': { slidesPerView: 1, spaceBetween: 2 },
+                    '@0.75': { slidesPerView: 2, spaceBetween: 2 },
+                    '@1.00': { slidesPerView: 3, spaceBetween: 2 },
+                    '@1.50': { slidesPerView: 6, spaceBetween: 2 },
+                }}
+                modules={[Pagination]}
+                className="mySwiper"
+            >
+                {Movies.map((Movie) => (
+                    <SwiperSlide key={Movie._id}>
+                        <MovieCard {...Movie} />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
                         )
                     })
                 }
